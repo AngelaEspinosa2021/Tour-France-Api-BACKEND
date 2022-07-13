4b6c914e0b57de163617b5943ee86cc98d61ba14
AngelaEspinosa2021 <titagena@gmail.com>
2022-07-13 16:38:23 -0500

    CREATE: Se crea la estructura base del proyecto en Java 11 con SpringBoot WEBFLUX + Reactivo.

---
+++ src/main/java/co/com/sofka/touroffranceapp/TourOfFranceAppApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.touroffranceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourOfFranceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourOfFranceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/sofka/touroffranceapp/TourOfFranceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.touroffranceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourOfFranceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
