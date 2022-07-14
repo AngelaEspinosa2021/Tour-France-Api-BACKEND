b34fcbd7e34793137fe3998a2b303f0c2ae49417
AngelaEspinosa2021 <titagena@gmail.com>
2022-07-14 09:01:29 -0500

    CREATE: Se hace prueba en la generación de numero aleatorio alfanumerico con 3 caracteres maximo.

--- src/main/java/co/com/sofka/touroffranceapp/TourOfFranceAppApplication.java
+++ src/main/java/co/com/sofka/touroffranceapp/TourOfFranceAppApplication.java
@@ -3,11 +3,15 @@ package co.com.sofka.touroffranceapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.UUID;
+
 @SpringBootApplication
 public class TourOfFranceAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TourOfFranceAppApplication.class, args);
+		//System.out.println(UUID.randomUUID().toString().toUpperCase().substring(0,3));
 	}
 
 }
