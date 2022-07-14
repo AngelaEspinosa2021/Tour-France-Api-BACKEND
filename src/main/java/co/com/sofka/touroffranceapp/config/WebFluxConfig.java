0ade0366613d0a3e461deea7b9a28309d43c5816
AngelaEspinosa2021 <titagena@gmail.com>
2022-07-14 12:03:52 -0500

    CREATE: Se crea la clase que administra las configuraciones como las CORS.

--- src/main/java/co/com/sofka/touroffranceapp/config/WebFluxConfig.java
+++ src/main/java/co/com/sofka/touroffranceapp/config/WebFluxConfig.java
@@ -1,2 +1,31 @@
-package co.com.sofka.touroffranceapp.config;public class WebFluxConfig {
+package co.com.sofka.touroffranceapp.config;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
+
+@Configuration
+public class WebFluxConfig {
+
+    @Bean
+    public WebFluxConfigurer corsConfigure(){
+        return new WebFluxConfigurerComposite(){
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")
+                        .allowedMethods("*");
+            }
+        };
+    }
+
+    @Bean
+    public WebProperties.Resources resources() {
+        return new WebProperties.Resources();
+    }
+
 }
