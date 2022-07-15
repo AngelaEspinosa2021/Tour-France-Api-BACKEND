d52cc2d9967199c1bef65091b2df16d55c244951
AngelaEspinosa2021 <titagena@gmail.com>
2022-07-15 07:48:44 -0500

    CREATE: Se crea la ruta dentro de la clase TeamRouter, se prueba funcionamiento con postman, todo funciona correctamente.

--- src/main/java/co/com/sofka/touroffranceapp/routers/TeamRouter.java
+++ src/main/java/co/com/sofka/touroffranceapp/routers/TeamRouter.java
@@ -2,9 +2,11 @@ package co.com.sofka.touroffranceapp.routers;
 
 import co.com.sofka.touroffranceapp.model.TeamDTO;
 import co.com.sofka.touroffranceapp.usecases.team.CreateTeamUseCase;
+import co.com.sofka.touroffranceapp.usecases.team.GetTeamUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
@@ -13,8 +15,7 @@ import java.awt.image.PackedColorModel;
 import java.util.function.Function;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
@@ -31,5 +32,18 @@ public class TeamRouter {
                POST("/team/create").and(accept(APPLICATION_JSON)),
                 request -> request.bodyToMono(TeamDTO.class)
                         .flatMap(createTeam));
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> get(GetTeamUseCase getTeamUseCase){
+        return route(
+                GET("/team/get/{id}").and(accept(APPLICATION_JSON)),
+                request -> ServerResponse.ok()
+                        .contentType(APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(getTeamUseCase.apply(
+                                request.pathVariable("id")),
+                                TeamDTO.class
+                        ))
+        );
     }
 }
